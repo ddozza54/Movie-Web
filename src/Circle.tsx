e5c8291b68a00ec08c06337021237c5651ee729a
UHquokka <bananamayong@gmail.com>
2023-02-24 21:34:52 +0900

    Props 의 type 정하기 :: interface

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,1 +1,21 @@
 import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: ${(props) => props.bgColor};
+`;
+interface CircleProps {
+  bgColor: string;
+}
+
+function Circle({ bgColor }: CircleProps) {
+  return <Container bgColor={bgColor}></Container>;
+}
+
+export default Circle;
