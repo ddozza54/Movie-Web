1173db703a7bdf1020cce973d9e1adf98bee1080
UHquokka <bananamayong@gmail.com>
2023-02-25 16:49:48 +0900

    #5.1 Router로 주소 만들기 & useParmas를 이용해 url 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { Route } from "react-router-dom";
 import styled from "styled-components";
+import Router from "./Router";
 
 function App() {
   const Container = styled.div`
@@ -9,11 +11,7 @@ function App() {
   const H1 = styled.h1`
     color: ${(props) => props.theme.textColor};
   `;
-  return (
-    <Container>
-      <H1>HEllo~~</H1>
-    </Container>
-  );
+  return <Router></Router>;
 }
 
 export default App;
--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,11 @@
 import { DefaultTheme } from "styled-components";
-export const lightTheme: DefaultTheme = {
+export const theme: DefaultTheme = {
   bgColor: "white",
   textColor: "black",
   btnColor: "tomato",
 };
 
-export const darkTheme: DefaultTheme = {
+const darkTheme: DefaultTheme = {
   bgColor: "black",
   textColor: "white",
   btnColor: "teal",
