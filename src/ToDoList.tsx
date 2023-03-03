70d4f1ab7a2c4992a1a6eb36657c8252c989c152
UHquokka <bananamayong@gmail.com>
2023-03-03 22:31:43 +0900

    react-hook-form 설치

---
+++ src/ToDoList.tsx
@@ -0,0 +1,12 @@
+import { useForm } from "react-hook-form";
+
+function ToDoList() {
+  return (
+    <div>
+      <form>
+        <input placeholder="Write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
